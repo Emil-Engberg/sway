5354caba15159c0958e912833366738010313e8e
Emil Engberg <emil.engberg@outlook.com>
2023-11-14 10:02:17 +0100

    Changed so that adaptive_sync command uses the return value from parse_boolean.

--- sway/commands/output/adaptive_sync.c
+++ sway/commands/output/adaptive_sync.c
@@ -10,32 +10,25 @@ struct cmd_results *output_cmd_adaptive_sync(int argc, char **argv) {
 	if (argc == 0) {
 		return cmd_results_new(CMD_INVALID, "Missing adaptive_sync argument");
 	}
-
+	
+	int current_value = 1;
+	
 	if(strcmp(argv[0], "toggle") == 0) {
 		struct output_config *oc = config->handler_context.output_config;
 		struct sway_output *sway_output = all_output_by_name_or_id(oc->name);
-
 		if (sway_output == NULL) {
 			return cmd_results_new(CMD_FAILURE,
 				"Cannot apply toggle to unknown output %s", oc->name);
 		}
-
 		oc = find_output_config(sway_output);
-
-		if (!oc || oc->adaptive_sync == 1) {
-			config->handler_context.output_config->adaptive_sync = 0;
-		} else {
-			config->handler_context.output_config->adaptive_sync = 1;
-		}
-
+		
+		current_value = !oc || oc->adaptive_sync;
+		
 		free(oc);
 	}
-	else if (parse_boolean(argv[0], true)) {
-		config->handler_context.output_config->adaptive_sync = 1;
-	} else {
-		config->handler_context.output_config->adaptive_sync = 0;
-	}
-
+	
+	config->handler_context.output_config->adaptive_sync = parse_boolean(argv[0], current_value);
+	
 	config->handler_context.leftovers.argc = argc - 1;
 	config->handler_context.leftovers.argv = argv + 1;
 	return NULL;
