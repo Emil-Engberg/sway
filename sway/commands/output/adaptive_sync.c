769a887e91a5363623928fa32b28d4d655723ac4
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2023-12-19 16:43:32 +0100

    removed spaces and replaced them with tabs in sway/commands/output/adaptive_sync.c

--- sway/commands/output/adaptive_sync.c
+++ sway/commands/output/adaptive_sync.c
@@ -4,30 +4,30 @@
 #include "util.h"
 
 struct cmd_results *output_cmd_adaptive_sync(int argc, char **argv) {
-    if (!config->handler_context.output_config) {
-        return cmd_results_new(CMD_FAILURE, "Missing output config");
-    }
-    if (argc == 0) {
-        return cmd_results_new(CMD_INVALID, "Missing adaptive_sync argument");
-    }
+	if (!config->handler_context.output_config) {
+		return cmd_results_new(CMD_FAILURE, "Missing output config");
+	}
+	if (argc == 0) {
+		return cmd_results_new(CMD_INVALID, "Missing adaptive_sync argument");
+	}
 
-    bool current_value = true;
-    
-    if (strcmp(argv[0], "toggle") == 0) {
-        struct output_config *oc = config->handler_context.output_config;
-        struct sway_output *sway_output = all_output_by_name_or_id(oc->name);
+	bool current_value = true;
+	
+	if (strcmp(argv[0], "toggle") == 0) {
+		struct output_config *oc = config->handler_context.output_config;
+		struct sway_output *sway_output = all_output_by_name_or_id(oc->name);
 
-        if (!sway_output || !sway_output->wlr_output) {
-            return cmd_results_new(CMD_FAILURE,
-                   "Cannot apply toggle to unknown output %s", oc->name);
-        }
-        current_value =
-            sway_output->wlr_output->adaptive_sync_status == WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED;
-    }
-    
-    config->handler_context.output_config->adaptive_sync = parse_boolean(argv[0], current_value);
-    
-    config->handler_context.leftovers.argc = argc - 1;
-    config->handler_context.leftovers.argv = argv + 1;
-    return NULL;
+		if (!sway_output || !sway_output->wlr_output) {
+			return cmd_results_new(CMD_FAILURE,
+				   "Cannot apply toggle to unknown output %s", oc->name);
+		}
+		current_value =
+			sway_output->wlr_output->adaptive_sync_status == WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED;
+	}
+	
+	config->handler_context.output_config->adaptive_sync = parse_boolean(argv[0], current_value);
+	
+	config->handler_context.leftovers.argc = argc - 1;
+	config->handler_context.leftovers.argv = argv + 1;
+	return NULL;
 }
